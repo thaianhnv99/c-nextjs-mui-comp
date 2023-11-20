1abc925beada2e43305d2c158433b68ffb2c5548
thaianhnv99 <thainhnguyen283@gmail.com>
2023-11-20 10:28:08 +0700

    add query key factory

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,5 +1,25 @@
 import { QueryClient } from "react-query";
 
+// Here is an example.
+// Create a const to summarize all the keys used with react-query, to avoid duplicate keys when working with react-query
+const getQueryKeys = (baseKey: string) => {
+  return {
+    all: [baseKey],
+    many: (params: Record<string, unknown>) => [baseKey, params],
+    one: (id: string) => [baseKey, id],
+  };
+};
+
+export const queryKeys = {
+  auth: {
+    authUser: ["get-info-user-key"],
+  },
+  jobs: getQueryKeys("jobs"),
+  organizations: {
+    one: getQueryKeys("organizations").one,
+  },
+};
+
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
